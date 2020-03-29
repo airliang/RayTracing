4a1aaae4449179534713aaaadcf03720a1f61ed2
airliang <air_liang1212@163.com>
2020-03-29 21:39:22 +0800

    Update diffusearealight.cpp

--- lights/diffusearealight.cpp
+++ lights/diffusearealight.cpp
@@ -2,5 +2,19 @@
 
 namespace AIR
 {
-	
+	Spectrum DiffuseAreaLight::Sample_Li(const Interaction &ref,
+        const Point2f &u, Vector3f *wi, Float *pdf,
+        VisibilityTester *vis) const
+    {
+        Interaction pShape = shape->Sample(ref, u, pdf);
+
+        if (*pdf == 0 || (pShape.interactPoint - ref.interactPoint).LengthSquared() == 0) 
+        {
+            *pdf = 0;
+            return 0.f;
+        }
+        *wi = Vector3f::Normalize(pShape.interactPoint - ref.interactPoint);
+        *vis = VisibilityTester(ref, pShape);
+        return L(pShape, -*wi);
+    }
 }
